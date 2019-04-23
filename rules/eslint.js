4d4606db04beec011eee3560ae78470b46da694d
Natan Sągol <m@merlinnot.com>
2019-04-23 09:13:29 +0200

    feat(typescript-eslint): enable rules introduced in v1.7.0

--- rules/eslint.js
+++ rules/eslint.js
@@ -7,6 +7,8 @@ module.exports = {
     'consistent-return': 'off',
     // Ignored properties in TypeScript consist of underscores.
     'id-length': ['error', { exceptions: ['_', '__', '___', '____'] }],
+    // Duplicate of @typescript-eslint/no-array-constructor.
+    'no-array-constructor': 'off',
     // Ternary operators are helpful to write immutable code.
     'no-nested-ternary': 'off',
     // Prettier automatically inserts semicolons, so there is no way to that
@@ -16,6 +18,8 @@ module.exports = {
     'no-ternary': 'off',
     // Only applicable to ECMAScript 3.
     'no-undefined': 'off',
+    // Duplicate of @typescript-eslint/no-unused-vars.
+    'no-unused-vars': 'off',
     // Duplicate of @typescript-eslint/no-use-before-define.
     'no-use-before-define': 'off',
     'one-var': ['error', 'never'],
