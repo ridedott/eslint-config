0d7dbc67db463dcfed33f39442436bcba5ffa64a
Natan Sągol <m@merlinnot.com>
2019-06-15 00:10:21 +0200

    feat(unicorn): add initial configuration

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ module.exports = {
     './rules/immutable.js',
     './rules/jest.js',
     './rules/typescript.js',
+    './rules/unicorn.js',
     'prettier',
     'prettier/@typescript-eslint',
     './rules/overrides.js',
   ],
-  plugins: ['@typescript-eslint', 'immutable', 'jest'],
+  plugins: ['@typescript-eslint', 'immutable', 'jest', 'unicorn'],
   parser: '@typescript-eslint/parser',
 };
