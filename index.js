c961ed4ee829cfe0ad743c6fbde12e8b250d82e8
Natan Sągol <m@merlinnot.com>
2019-06-15 00:25:23 +0200

    feat(simple-import-sort): add initial configuration

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ module.exports = {
     './rules/eslint.js',
     './rules/immutable.js',
     './rules/jest.js',
+    './rules/simple-import-sort.js',
     './rules/typescript.js',
     'prettier',
     'prettier/@typescript-eslint',
     './rules/overrides.js',
   ],
-  plugins: ['@typescript-eslint', 'immutable', 'jest'],
+  plugins: ['@typescript-eslint', 'immutable', 'jest', 'simple-import-sort'],
   parser: '@typescript-eslint/parser',
 };
---
+++ rules/simple-import-sort.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'simple-import-sort/sort': 'error',
+  },
+};
