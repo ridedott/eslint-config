df26ba611822c7d0f6d3e16404abb9edd1202533
Derek Yu <derekyu624@hotmail.com>
2019-06-20 12:47:43 +0200

    feat(prettier): add prettier plugin for eslint

---
+++ rules/prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        endOfLine: 'lf',
+        proseWrap: 'always',
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
