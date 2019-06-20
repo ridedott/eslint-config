7790161a681d6768714ce0a3635777f8c2244ad9
Derek Yu <derekyu624@hotmail.com>
2019-06-20 11:33:00 +0200

    feat(prettier): specify explicit prettier configuration in rules

---
+++ rules/prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        proseWrap: 'always',
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
