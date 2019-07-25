a45281e26668f3454de535e099df1cde4d289169
Natan Sągol <m@merlinnot.com>
2019-07-25 14:57:59 +0200

    fix(typescript): disable conflicting require-await rule

--- rules/typescript.js
+++ rules/typescript.js
@@ -15,6 +15,8 @@ module.exports = {
       { ignoreRestSiblings: true },
     ],
     '@typescript-eslint/promise-function-async': ['error', { allowAny: true }],
+    // Conflicts with @typescript-eslint/promise-function-async.
+    '@typescript-eslint/require-await': 'off',
     // Code should be formatted using Prettier.
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
