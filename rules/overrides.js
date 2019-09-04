b74a0f40514535b712ff2f8d6ecad32bfcfb8ad8
Natan Sągol <m@merlinnot.com>
2019-09-04 12:47:55 +0200

    feat: ignore URLs in max-len rule

--- rules/overrides.js
+++ rules/overrides.js
@@ -2,6 +2,6 @@ module.exports = {
   rules: {
     // Prettier automatically formats code, but can't format comments just yet:
     // https://github.com/prettier/prettier/issues/265
-    'max-len': ['error', { code: 1024, comments: 80 }],
+    'max-len': ['error', { code: 1024, comments: 80, ignoreUrls: true }],
   },
 };
