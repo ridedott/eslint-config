c879844500b8d686cd60909aba1304dc4f7a49a0
Joram <joram.ruitenschild@gmail.com>
2019-09-05 16:44:15 +0200

    fix: enable other prefer-to-* rules

--- rules/jest.js
+++ rules/jest.js
@@ -7,6 +7,9 @@ module.exports = {
     'jest/no-try-expect': 'off',
     'jest/prefer-called-with': 'off',
     'jest/prefer-inline-snapshots': 'off',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-be-undefined': 'error',
+    'jest/prefer-to-contain': 'error',
     'jest/prefer-to-have-length': 'error',
   },
 };
