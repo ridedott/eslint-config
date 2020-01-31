cc8c58e53d73a689a4f7df30bcd1acf9d3d35289
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-01-31 14:59:41 +0100

    fix: refactor tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,18 +1,25 @@
-import * as fixtures from './fixtures';
+import { rules as typescriptEslintRules } from '@typescript-eslint/eslint-plugin';
+import { CLIEngine, Linter } from 'eslint';
 import { rules as arrayFuncRules } from 'eslint-plugin-array-func';
 import { rules as eslintCommentsRules } from 'eslint-plugin-eslint-comments';
 import { rules as immutableRules } from 'eslint-plugin-immutable';
 import { rules as jestRules } from 'eslint-plugin-jest';
 import { rules as simpleImportSortRules } from 'eslint-plugin-simple-import-sort';
-import { rules as typescriptEslintRules } from '@typescript-eslint/eslint-plugin';
 import { rules as unicornRules } from 'eslint-plugin-unicorn';
-import { Linter, CLIEngine } from 'eslint';
 
 import * as ourRules from '../rules';
+import * as fixtures from './fixtures';
 
 /*
  * Test utilities
  */
+
+ interface VerifyResponse {
+  [key: string]: unknown,
+  warningCount: number,
+  messages: [],
+  errorCount: number,
+ }
 
 const cli = new CLIEngine();
 const linter = new Linter();
@@ -31,34 +38,39 @@ const ourRulesToOriginalMap = {
   unicorn: unicornRules,
 };
 
-const verifyFixture = (
-  ruleSet: string,
-  ruleName: string,
-  ourRule: string,
-  type: 'pass' | 'fail',
-) => {
-  if (fixtures[ruleSet][ruleName] == undefined) {
+const verifyFixture = ({
+  ruleSet,
+  ruleName,
+  ourRule,
+  type,
+}: {
+  ruleSet: string;
+  ruleName: string;
+  ourRule: string;
+  type: 'pass' | 'fail';
+}): VerifyResponse => {
+  if (fixtures[ruleSet][ruleName] === undefined) {
     throw new Error(`Missing fixtures for ${ruleSet}/${ruleName}`);
   }
 
   const fixture = fixtures[ruleSet][ruleName][type];
   const report = cli.executeOnText(
     fixture,
-    ruleSet !== 'eslint'
-      ? `test/fixtures/${ourRule}.${type}.ts`
-      : `test/fixtures/eslint/${ourRule}.${type}.ts`,
+    ruleSet === 'eslint'
+      ? `test/fixtures/eslint/${ourRule}.${type}.ts`
+      : `test/fixtures/${ourRule}.${type}.ts`,
   );
 
   return report.results[0];
 };
 
-const getRuleName = ourRule => {
+const getRuleName = (ourRule: string): string => {
   /**
    * Rules from plugins are prefixed but rules from Eslint are not.
    */
   const regex = /(?:.*)\/(?<prefixed>.*)|(?<unPrefixed>.*)/u;
 
-  const match = ourRule.match(regex);
+  const match = regex.exec(ourRule);
 
   if (match === null) {
     return;
@@ -78,30 +90,53 @@ const getRuleName = ourRule => {
 /*
  * Tests
  */
-describe.each(Object.keys(ourRules))('%s rules', ruleSet => {
-  describe.each(Object.keys(ourRules[ruleSet].rules))('%s', ourRule => {
-    const ruleName = getRuleName(ourRule);
-    if (ourRules[ruleSet].rules[ourRule] === 'off' || ruleSet !== 'eslintComments') {
-      return;
-    }
+describe.each(Object.keys(ourRules))('%s rules', (ruleSet: string): void => {
+  describe.each(Object.keys(ourRules[ruleSet].rules))(
+    '%s',
+    (ourRule: string): void => {
+      const ruleName = getRuleName(ourRule);
 
-    it(`should exist in the current version of ${ruleSet}`, () => {
-      expect(ourRulesToOriginalMap[ruleSet][ruleName]).not.toBeUndefined();
-    });
+      if (
+        ourRules[ruleSet].rules[ourRule] === 'off' ||
+        ruleSet !== 'eslintComments'
+      ) {
+        return;
+      }
 
-    it('should pass', () => {
-      const result = verifyFixture(ruleSet, ruleName, ourRule, 'pass');
+      it(`should exist in the current version of ${ruleSet}`, (): void => {
+        expect.assertions(1);
 
-      expect(result.warningCount).toStrictEqual(0);
-      expect(result.messages).toStrictEqual([]);
-      expect(result.errorCount).toStrictEqual(0);
-    });
+        expect(ourRulesToOriginalMap[ruleSet][ruleName]).not.toBeUndefined();
+      });
 
-    it('should fail', () => {
-      const result = verifyFixture(ruleSet, ruleName, ourRule, 'fail');
+      it('should pass', (): void => {
+        expect.assertions(3);
 
-      expect(result.warningCount).toStrictEqual(0);
-      expect(result.errorCount).toBeGreaterThan(0);
-    });
-  });
+        const result = verifyFixture({
+          ourRule,
+          ruleName,
+          ruleSet,
+          type: 'pass',
+        });
+
+        expect(result.warningCount).toStrictEqual(0);
+        expect(result.messages).toStrictEqual([]);
+        expect(result.errorCount).toStrictEqual(0);
+      });
+
+      it('should fail', (): void => {
+        expect.assertions(2);
+
+        const result = verifyFixture({
+          ourRule,
+          ruleName,
+          ruleSet,
+          type: 'fail',
+        });
+
+        expect(result.warningCount).toStrictEqual(0);
+        expect(result.errorCount).toBeGreaterThan(0);
+      });
+    },
+  );
 });
