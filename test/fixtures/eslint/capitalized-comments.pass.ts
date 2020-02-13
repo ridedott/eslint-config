68041318ef314e1c3de3cf798295a0241d9730b0
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-02-13 14:18:33 +0100

    fix: add review comments

--- test/fixtures/eslint/capitalized-comments.pass.ts
+++ test/fixtures/eslint/capitalized-comments.pass.ts
@@ -1,5 +1,12 @@
-// Capitalized comment
+/**
+ * Capitalized comment
+ */
 
-// 1. Non-letter at beginning of comment
+/**
+ * 1. Non-letter at beginning of comment
+ */
 
-// 丈 Non-Latin character at beginning of comment
+/**
+ * 丈 Non-Latin character at beginning of comment
+ */
+
