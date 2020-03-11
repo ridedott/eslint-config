d3f9a314dc7bcc0da1b431ded516464e04421a1c
Natan Sągol <m@merlinnot.com>
2020-03-11 16:58:30 +0100

    add tests

---
+++ test/fixtures/@typescript-eslint/no-base-to-string.fail.ts
@@ -0,0 +1,3 @@
+const argument1 = { 1: 2 };
+
+export const message1 = `arg1 = ${argument1.toString()}`;
---
+++ test/fixtures/@typescript-eslint/no-base-to-string.pass.ts
@@ -0,0 +1,3 @@
+const argument1 = [1];
+
+export const message1 = `arg1 = ${argument1.toString()}`;
