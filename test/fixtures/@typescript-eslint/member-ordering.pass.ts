6e8e8d91a3b8a1bf95d169ad6f5b974db60177f0
Natan Sągol <m@merlinnot.com>
2020-04-11 18:28:41 +0200

    fix: restore default member ordering

--- test/fixtures/@typescript-eslint/member-ordering.pass.ts
+++ test/fixtures/@typescript-eslint/member-ordering.pass.ts
@@ -1,4 +1,24 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable @typescript-eslint/prefer-readonly */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable lines-between-class-members */
+
 interface Test {
   a: undefined;
   b: undefined;
+}
+
+class Test2 {
+  public static b: undefined;
+  protected static c: undefined;
+  private static a: undefined;
+  constructor() {}
+  static cm() {}
+  am() {}
+  bm() {}
+  private dm() {}
 }
