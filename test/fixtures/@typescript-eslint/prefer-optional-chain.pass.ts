6dcda0ea819274aca08b8bdf2315241e918c6637
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-06-08 13:45:40 +0200

    fix: update passing fixtures

--- test/fixtures/@typescript-eslint/prefer-optional-chain.pass.ts
+++ test/fixtures/@typescript-eslint/prefer-optional-chain.pass.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 
-interface Test {
+interface OptionalChainPass {
   [key: string]: any;
 }
-const preferOptionalChainPass: Test = {};
+const preferOptionalChainPass: OptionalChainPass = {};
 
 preferOptionalChainPass?.a?.b?.c;
 preferOptionalChainPass?.['a']?.b?.c;
