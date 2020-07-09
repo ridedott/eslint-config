f4c7eb5892fc909881c8144e8deed83235e3f8b0
Ed <oleg@ridedott.com>
2020-07-09 13:46:05 +0200

    fix: ignore cspell for no-single-line-comment-block

--- rules/ridedott.js
+++ rules/ridedott.js
@@ -1,6 +1,11 @@
 module.exports = {
   rules: {
-    '@ridedott/no-single-line-comment-block': 'error',
+    '@ridedott/no-single-line-comment-block': [
+      'error',
+      {
+        ignore: ['cspell'],
+      },
+    ],
     '@ridedott/no-template-literals-without-expression': [
       'error',
       { ignoreMultiline: true },
