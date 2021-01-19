bb87339d2d626935bc5fb1b0064416771650b2d1
Algin Maduro <algin.maduro@gmail.com>
2021-01-19 16:52:36 +0100

    fix: extend expired todo

--- test/fixtures/unicorn/prefer-flat-map.fail.ts
+++ test/fixtures/unicorn/prefer-flat-map.fail.ts
@@ -1,7 +1,7 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 
 /*
- * TODO (viestat) [2020-12-25]: Figure out why this rule breaks whe using:
+ * TODO (viestat) [2020-03-01]: Figure out why this rule breaks whe using:
  * testForMapFail.map((el) => el).flat();
  * Maybe it is due to a collision with array-func/prefer-flat-map.
  */
