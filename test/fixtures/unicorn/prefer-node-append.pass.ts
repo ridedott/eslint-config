d366d3dca80d48134900d5c9789b527ed99050d0
Joram <joram.ruitenschild@gmail.com>
2021-06-14 10:10:08 +0100

    fix: todo

--- test/fixtures/unicorn/prefer-node-append.pass.ts
+++ test/fixtures/unicorn/prefer-node-append.pass.ts
@@ -1,4 +1,4 @@
 /**
  * cspell:ignore viestat
- * TODO (viestat) [2021-05-31]: Find a good way to test as this is for browser.
+ * TODO (viestat) [2021-07-01]: Find a good way to test as this is for browser.
  */
