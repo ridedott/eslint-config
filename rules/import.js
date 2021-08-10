53cbc4a8ee8ecdbedd85280b3a20d44b2778da77
Algin Maduro <1469047+mad-it@users.noreply.github.com>
2021-08-10 13:25:51 +0200

    fix: disable import/namespace rule

--- rules/import.js
+++ rules/import.js
@@ -1,5 +1,7 @@
 module.exports = {
   rules: {
+    // This rule conflicts with features provided by TypeScript.
+    'import/namespace': 'off',
     'import/no-cycle': ['error', { maxDepth: Infinity }],
     'import/no-default-export': 'error',
     'import/no-self-import': 'error',
