bf38a98ebc7f1b7d08ab5eac5bab69cdf0ab8951
Anna Aneychik <anna.aneychik@ridedott.com>
2021-08-21 07:46:05 +0200

    revert: perf: do not detect cycles in dependencies
    This reverts commit 7cbf043d241f88a85f722b567f692662dab879cb.

--- rules/import.js
+++ rules/import.js
@@ -2,7 +2,7 @@ module.exports = {
   rules: {
     // This rule conflicts with features provided by TypeScript.
     'import/namespace': 'off',
-    'import/no-cycle': ['error', { ignoreExternal: true, maxDepth: Infinity }],
+    'import/no-cycle': ['error', { maxDepth: Infinity }],
     'import/no-default-export': 'error',
     'import/no-self-import': 'error',
   },
