4440ca32c9bb5e064d765272520b993dec2e1cde
abramenal <pavel.auramenka@gmail.com>
2021-12-28 17:18:29 +0100

    feat: add import/no-extraneous-dependencies rule

--- rules/import.js
+++ rules/import.js
@@ -5,5 +5,14 @@ module.exports = {
     'import/no-cycle': ['error', { maxDepth: Infinity }],
     'import/no-default-export': 'error',
     'import/no-self-import': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+        bundledDependencies: false,
+      },
+    ],
   },
 };
