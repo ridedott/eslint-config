daaf159f443551e600a1d251d32b82d62f4ee137
abramenal <pavel.auramenka@gmail.com>
2022-01-19 15:52:53 +0100

    test: enable rule in test files

--- test/fixtures/import/no-extraneous-dependencies.fail.ts
+++ test/fixtures/import/no-extraneous-dependencies.fail.ts
@@ -1,1 +1,3 @@
+/* eslint-disable eslint-comments/no-use, max-len */
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": false, "optionalDependencies": false, "bundledDependencies": false }] */
 export * as plugin from 'npm-run-all';
