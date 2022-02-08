9d78808d947a96daaa0cacfeb6d1ed8d7011d3c6
Anna Aneychik <anna.aneychik@ridedott.com>
2022-02-08 18:11:49 +0100

    chore: add test for prefer-add-event-listener

--- test/fixtures/unicorn/prefer-add-event-listener.pass.ts
+++ test/fixtures/unicorn/prefer-add-event-listener.pass.ts
@@ -1,4 +1,2 @@
-/**
- * cspell:ignore viestat
- * TODO (viestat) [2022-04-01]: Find a good way to test this
- */
+const button = {} as HTMLButtonElement;
+button.addEventListener('click', (): void => {});
