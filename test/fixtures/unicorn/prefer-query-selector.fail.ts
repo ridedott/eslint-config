b39764b8e01685c36888e5809333fc135a5f93b8
Anna Aneychik <anna.aneychik@ridedott.com>
2022-02-08 18:11:36 +0100

    chore: add test for prefer-query-selector

--- test/fixtures/unicorn/prefer-query-selector.fail.ts
+++ test/fixtures/unicorn/prefer-query-selector.fail.ts
@@ -1,7 +1,1 @@
-/**
- * cspell:ignore viestat
- * TODO (viestat) [2022-04-01]: Find a good way to test as this is for browser.
- * The fixture bellow fails for a different reason on purpose.
- */
-
-const hereTofailThisFixture2 = 0 << 1;
+document.getElementById("foo");
