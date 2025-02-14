42bda7ddcf34aafb495eadc3be5e7641eaf23512
Anna Aneychik <anna.aneychik@ridedott.com>
2025-02-14 15:18:48 +0100

    chore: use vitest instead of jest

--- babel.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  parserOpts: { strictMode: true },
-  plugins: ['@babel/plugin-transform-typescript'],
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
-};
