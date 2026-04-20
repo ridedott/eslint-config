72865d560ad4512fe2bc1ff354e26d5f6008101b
Joram Ruitenschild <joram.ruitenschild@gmail.com>
2026-04-20 12:18:56 +0100

    fix: update test file paths

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const TEST_FILES = [
-  '**/test/**',
-  '**/__mocks__/**',
-  '**/*.spec.*',
-  '**/*.test.*',
+  '**/test/**/*.{ts,tsx,mts,cts}',
+  '**/__mocks__/**/*.{ts,tsx,mts,cts}',
+  '**/*.spec.{ts,tsx,mts,cts}',
+  '**/*.test.{ts,tsx,mts,cts}',
 ];
