88b34ff5a12c08fef41a661e8254e7fe5523516f
Joram Ruitenschild <joram.ruitenschild@gmail.com>
2026-04-20 09:37:57 +0100

    fix: tweaks

--- configs/vitest.js
+++ configs/vitest.js
@@ -11,24 +11,21 @@ export default [
     rules: {
       ...vitestPlugin.configs.all.rules,
       'vitest/consistent-each-for': 'off',
-      'vitest/consistent-test-filename': 'off',
-      'vitest/consistent-test-it': [
-        'error',
-        { fn: 'it', withinDescribe: 'it' },
-      ],
-      'vitest/expect-expect': 'off',
-      // Can be safely turned off thanks to vitest/prefer-expect-assertions.
-      'vitest/no-conditional-expect': 'off',
+      ///// 'vitest/consistent-test-filename': 'off',
+      ///// 'vitest/consistent-test-it': [
+      /////   'error',
+      /////   { fn: 'it', withinDescribe: 'it' },
+      ///// ],
+      ///// 'vitest/expect-expect': 'off',
+      ///// // Can be safely turned off thanks to vitest/prefer-expect-assertions.
+      ///// 'vitest/no-conditional-expect': 'off',
       'vitest/no-disabled-tests': 'error',
       'vitest/no-focused-tests': 'error',
-      'vitest/no-hooks': 'off',
+      ///// 'vitest/no-hooks': 'off',
       'vitest/no-identical-title': 'error',
-      'vitest/hoisted-apis-on-top': 'off',
-      // Needs an explicit limit to be useful.
       'vitest/max-expects': 'off',
-      // Needs an explicit limit to be useful.
       'vitest/no-large-snapshots': 'off',
-      // Conflicts with padding-line-between-statements.
+      // Redundant with @stylistic/padding-line-between-statements, which already enforces blank lines around block-like statements.
       'vitest/padding-around-after-all-blocks': 'off',
       'vitest/padding-around-after-each-blocks': 'off',
       'vitest/padding-around-all': 'off',
@@ -39,7 +36,7 @@ export default [
       'vitest/padding-around-test-blocks': 'off',
       'vitest/prefer-describe-function-title': 'off',
       'vitest/prefer-each': 'off',
-      'vitest/prefer-expect-assertions': 'error',
+      ///// 'vitest/prefer-expect-assertions': 'error',
       // Too restrictive for simple flat test files.
       'vitest/require-hook': 'off',
       // Too restrictive for simple flat test files.
@@ -47,7 +44,7 @@ export default [
       'vitest/valid-describe-callback': 'error',
       'vitest/valid-expect': 'error',
       'vitest/valid-expect-in-promise': 'error',
-      'vitest/valid-title': ['error', { disallowedWords: ['should'] }],
+      ///// 'vitest/valid-title': ['error', { disallowedWords: ['should'] }],
     },
     settings: {
       vitest: {
